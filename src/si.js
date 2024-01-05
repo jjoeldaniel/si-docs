ba648a0f4205d291162e32a5eff74ef59a06df06
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-01-05 12:38:51 -0800

    add wip dropdown

---
+++ src/si.js
@@ -0,0 +1,10 @@
+export class SI_Leader {
+    constructor(name, days, time, location, zoom = "null", discord = "null") {
+        this.name = name;
+        this.days = days;
+        this.time = time;
+        this.location = location;
+        this.zoom = zoom;
+        this.discord = discord;
+    }
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
 	theme: {
 		extend: {},
 		colors: {
@@ -14,5 +17,6 @@ module.exports = {
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
+    require('flowbite/plugin'),
 	],
 }
